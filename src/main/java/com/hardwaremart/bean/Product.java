8b170727fdcd54a732e06bbf7517b0c9a93c17c3
pawan <meanstack4@gmail.com>
2020-10-14 13:41:07 +0530

    adding getter method in product bean

--- src/main/java/com/hardwaremart/bean/Product.java
+++ src/main/java/com/hardwaremart/bean/Product.java
@@ -1,5 +1,20 @@
 package com.hardwaremart.bean;
 
 public class Product {
-
+   private String id;
+   private String name;
+   private String description;
+   private int price;
+   public String getId(){
+     return id;
+   }
+   public String getName(){
+     return name;
+   }
+   public String getDescription(){
+     return description;
+   }
+   public int getPrice(){
+     return price;
+   }
 }
