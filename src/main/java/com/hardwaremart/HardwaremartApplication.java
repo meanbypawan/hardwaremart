5a61d3f2b9b9e5c0b40e8d37b8b04796195687c4
meanstack4@gmail.com <meanstack4@gmail.com>
2020-10-14 12:16:54 +0530

    initial commit

---
+++ src/main/java/com/hardwaremart/HardwaremartApplication.java
@@ -0,0 +1,13 @@
+package com.hardwaremart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwaremartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwaremartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardwaremart/HardwaremartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardwaremart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwaremartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
